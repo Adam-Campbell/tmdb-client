e8ed1fd6aa295ecb51423456bf8f0adac7360a69
Adam <adamcampbelldev@gmail.com>
2019-07-25 22:02:13 +0100

    Resolved subnav bug

--- server/personRoutes.js
+++ server/personRoutes.js
@@ -1,21 +1,22 @@
 const router = require('express').Router();
+const { normalizeUrl } = require('./utils');
 
 function personRoutes(app) {
     router.route('/:id')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/person';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
     router.route('/:id/images')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/person/images';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
 
     router.route('/:id/credits')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/person/credits';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
--- server/showRoutes.js
+++ server/showRoutes.js
@@ -1,59 +1,63 @@
 const router = require('express').Router();
+const { normalizeUrl } = require('./utils');
 
 function showRoutes(app) {
+
     router.route('/:id')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
     
-        router.route('/:id/recommended')
-        .get((req, res) => {
+    router.route('/:id/recommended')
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/recommended';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
+
     router.route('/:id/similar')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/similar';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
+        
     router.route('/:id/cast-and-crew')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/cast-and-crew';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
+
     router.route('/:id/reviews')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/reviews';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
+
     router.route('/:id/images')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/images';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
 
     router.route('/:id/seasons')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/seasons';
             const params = { id: req.params.id };
             app.render(req, res, url, params);
         })
 
     router.route('/:id/season/:number')
-        .get((req, res) => {
+        .get(normalizeUrl, (req, res) => {
             const url = '/show/season';
             const params = { id: req.params.id, number: req.params.number };
             app.render(req, res, url, params);
         })
-    // more subroutes to be added later
-
     
     return router;
 }
