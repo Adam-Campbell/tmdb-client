371bd7699c707e1f2f898cba0beed48c910e2a97
Adam <adamcampbelldev@gmail.com>
2019-07-31 17:52:42 +0100

    Added lists route

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,3 +1,4 @@
 export * from './SeasonCard';
 export * from './MinimalCard';
 export * from './MediaCard';
+export * from './ListCard';
---
+++ pages/me/lists.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { getUsersCreatedLists } from '../../reducers/user';
+import { fetchFullProfile } from '../../actions';
+import SubNav from '../../components/SubNav';
+import { meRoutesSubNavData } from './';
+import UserHeader from '../../components/UserHeader';
+import { Row } from '../../components/Layout';
+import { ListCard } from '../../components/Cards';
+import ListViewHeader from '../../components/ListViewHeader';
+
+const ListCardsContainer = styled(Row)`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+function Lists({ lists }) {
+    return (
+        <div>
+            <UserHeader />
+            <SubNav navData={meRoutesSubNavData} alignLeft={true} />
+            <ListViewHeader title="My Lists">
+            </ListViewHeader>
+            <ListCardsContainer>
+                {lists.map(list => (
+                    <ListCard 
+                        key={list.id}
+                        listId={list.id}
+                        imagePath={list.poster_path}
+                        name={list.name}
+                        itemCount={list.item_count}
+                    />
+                ))}
+            </ListCardsContainer>
+        </div>
+    );
+}
+
+Lists.getInitialProps = async ({ query, store }) => {
+    await store.dispatch(fetchFullProfile());
+    return {};
+}
+
+function mapState(state) {
+    return {
+        lists: getUsersCreatedLists(state)
+    };
+}
+
+export default connect(mapState)(Lists);
