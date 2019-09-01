1a14b980c380966611f855b7d2198d04ac5628e2
Adam <adamcampbelldev@gmail.com>
2019-09-01 19:17:03 +0100

    Fixed bug in subnavs that occurred after adding pagination to some routes

--- client/polyfills.js
+++ client/polyfills.js
@@ -3,4 +3,5 @@ import 'core-js/features/array/find';
 import 'core-js/features/array/find-index';
 import 'core-js/features/array/includes';
 import 'core-js/features/array/some';
+import 'core-js/features/string/starts-with';
 require('intersection-observer');
